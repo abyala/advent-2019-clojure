d5d3161ec52f2bba2fdf6eb15d35da02638f6c33
Andrew Byala <abyala@yahoo.com>
2022-04-15 21:15:46 -0500

    Day 12 needs cleanup and write-up

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject advent-2019-clojure "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.10.3"]
                  [org.clojure/core.async "1.3.622"]
-                 [org.clojure/math.combinatorics "0.1.6"]]
+                 [org.clojure/math.combinatorics "0.1.6"]
+                 [org.clojure/math.numeric-tower "0.0.5"]]
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
                        :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
--- src/advent_2019_clojure/day12.clj
+++ src/advent_2019_clojure/day12.clj
@@ -1,6 +1,7 @@
 (ns advent-2019-clojure.day12
   (:require [advent-2019-clojure.utils :refer [parse-long abs]]
             [clojure.math.combinatorics :as combo]
+            [clojure.math.numeric-tower :as math]
             [clojure.string :as str]))
 
 (def all-dimensions [:x :y :z])
@@ -54,3 +55,68 @@
                              (conj seen state)))
           #{}
           (iterate take-step (parse-input input))))
+
+(slurp "resources/day12_puzzle.txt")
+(defn parse-input2 [input]
+  (let [lines (str/split-lines input)
+        num-lines (count lines)]
+    (->> lines
+         (mapv (fn [line] (->> line
+                               (re-seq #"-?\d+")
+                               (mapv parse-long))))
+         (apply interleave)
+         (partition num-lines)
+         (map #(vector (vec %) (vec (repeat num-lines 0)))))))
+
+(defn move-moons [positions velocities]
+  (let [velocity-changes (map (comp (partial reduce +)
+                                    (fn [pos] (map #(compare % pos) positions)))
+                              positions)
+        new-velocities (mapv + velocities velocity-changes)
+        new-positions (mapv + positions new-velocities)]
+    [new-positions new-velocities]))
+
+(defn take-step2 [moons]
+  (map #(apply move-moons %) moons))
+
+(defn energy-calc [f moons]
+  (->> moons
+       (map f)
+       (apply interleave)
+       (map abs)
+       (partition (count moons))
+       (map (partial apply +))))
+(def potential-energy2 (partial energy-calc first))
+(def kinetic-energy2 (partial energy-calc second))
+
+(defn total-energy2 [moons]
+  (->> moons
+       ((juxt potential-energy2 kinetic-energy2))
+       (apply (partial map *))
+       (reduce +)))
+
+(defn moon-movement-seq [moons]
+  (iterate (partial map (partial apply move-moons)) moons))
+
+(defn part1-2 [input num-steps]
+  (-> input parse-input2 moon-movement-seq (nth num-steps) total-energy2))
+
+(defn first-cycle [dim-seq]
+  (reduce (fn [seen state] (if (seen state)
+                             (reduced (count seen))
+                             (conj seen state)))
+          #{}
+          dim-seq))
+
+(defn lcm-rf
+  ([] 1)
+  ([result] result)
+  ([result input] (math/lcm result input)))
+
+(defn part2 [input]
+  (let [moon-seq (-> input parse-input2 moon-movement-seq)]
+    (transduce (map (fn [dim] (->> moon-seq
+                                   (map #(nth % dim))
+                                   first-cycle)))
+               lcm-rf
+               (range 3))))
